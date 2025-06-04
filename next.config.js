1f0c733aa42ad7d027218ecf9376e6a8a6e3d06a
Ankur Singh Jethuri <131728079+ankursinghjethuri@users.noreply.github.com>
2025-06-04 07:58:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  assetPrefix: "/forex/",
-  basePath: "/forex",
+  images: {
+    unoptimized: true
+  }
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
