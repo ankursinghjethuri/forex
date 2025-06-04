8d8aa0316c07cf1aefd136e7da56412322e528e1
Ankur Singh Jethuri <131728079+ankursinghjethuri@users.noreply.github.com>
2025-06-04 07:05:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '5rem' }}>
+      <h1>Welcome to TrendyAI Forex</h1>
+      <p>Smart signals. Smarter trading.</p>
+    </div>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,10 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: "/forex/",
+  basePath: "/forex",
+}
+
+module.exports = nextConfig;
